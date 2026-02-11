6ab8b58187c3c84c2675035531d9466888f3cf3c
João Mourato <joaomouratocn@gmail.com>
2026-02-11 00:24:39 -0300

    update desgin category page

--- app/src/app/pages/category-page/category-page.ts
+++ app/src/app/pages/category-page/category-page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CategoryResponseInterface } from '../../models/interfaces/categoty-response-interface';
 import { CategoryService } from '../../services/category-service';
@@ -11,8 +11,10 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
   styleUrl: './category-page.css',
 })
 export class CategoryPage {
+  @ViewChildren('row') rows!: QueryList<ElementRef<HTMLTableRowElement>>;
   categorySearch: string = '';
   categoryList: CategoryResponseInterface[] = [];
+  selectedIndex = 0;
 
   constructor(
     private categoryService: CategoryService,
@@ -29,5 +31,64 @@ export class CategoryPage {
         console.log(err?.error.message);
       },
     });
+  }
+
+  onTableFocus() {
+    this.focusRow(this.selectedIndex);
+  }
+
+  focusRow(index: number) {
+    this.rows.get(index)?.nativeElement.focus();
+  }
+
+  selectRow(index: number) {
+    this.selectedIndex = index;
+    this.focusRow(index);
+  }
+
+  onKeyDown(event: KeyboardEvent, index: number) {
+    switch (event.key) {
+      case 'ArrowDown':
+        event.preventDefault();
+        this.moveDown(index);
+        break;
+
+      case 'ArrowUp':
+        event.preventDefault();
+        this.moveUp(index);
+        break;
+
+      case 'Enter':
+        event.preventDefault();
+        this.edit(this.categoryList[index]);
+        break;
+    }
+  }
+
+  moveDown(index: number) {
+    if (index < this.rows.length - 1) {
+      this.selectedIndex = index + 1;
+      this.focusRow(this.selectedIndex);
+    }
+  }
+
+  moveUp(index: number) {
+    if (index > 0) {
+      this.selectedIndex = index - 1;
+      this.focusRow(this.selectedIndex);
+    }
+  }
+
+  onEnter(index: number) {
+    const item = this.categoryList[index];
+    this.edit(item);
+  }
+
+  edit(category: CategoryResponseInterface) {
+    console.log('EDIT');
+  }
+
+  remove(category: CategoryResponseInterface) {
+    console.log('REMOVE');
   }
 }
